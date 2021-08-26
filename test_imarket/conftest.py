5be5f39c92102a9fbef94750abdee7aa3a914266
Way8ne8 <87656300+Way8ne8@users.noreply.github.com>
2021-08-26 14:16:28 +0300

    clear_code

--- test_imarket/conftest.py
+++ test_imarket/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 
 
 def pytest_addoption(parser):
--- test_imarket/test_items.py
+++ test_imarket/test_items.py
@@ -1,11 +1,6 @@
-import pytest
-from selenium import webdriver
 import time
-import math
-
 
 def test_basket_button(browser):
-
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(10)
     basket_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
