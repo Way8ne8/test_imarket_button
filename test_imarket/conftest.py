2a9d4541abf12bbcd4881a957a3482dd5d315596
Way8ne8 <87656300+Way8ne8@users.noreply.github.com>
2021-08-26 14:04:28 +0300

    imarket_test_for_Stepik

---
+++ test_imarket/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru, en, fr, ...")
+
+languages = ['ar', 'ca', 'cs', 'da', 'de', 'en-gb', 'el', 'es', 'fi', 'fr', 'it',
+             'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-hans']
+
+@pytest.fixture(scope="function")
+def browser(request):
+    lang_option = request.config.getoption("language")
+    if lang_option in languages:
+        options = webdriver.ChromeOptions()
+        user_language = request.config.getoption("language")
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--language should be ru, en, fr, ... etc")
+    browser.implicitly_wait(10)
+    yield browser
+    browser.quit()
---
+++ test_imarket/test_items.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+
+
+def test_basket_button(browser):
+
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(10)
+    basket_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    result = False
+    if len(basket_button) == 1:
+        result = True
+    assert result, "А где же кнопочка??? Или их так много..."
